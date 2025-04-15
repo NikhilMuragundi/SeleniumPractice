6098e205f6a1ed25365b63388ee99dc9a5effe6d
Nikhil Muragundi <NI20457047@wipro.com>
2025-04-15 16:50:30 +0530

    fixed bugs

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -1,9 +1,15 @@
 package TestRunner;
 
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+//import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+
+import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
+
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src\\test\\java\\Resources",
         glue = {"StepDefinition"},
